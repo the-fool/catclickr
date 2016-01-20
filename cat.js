4fe31851fa0754150378b2e3f03ff05292d340b5
the-fool <sketchbang@gmail.com>
2016-01-19 18:35:38 -0600

    mvc refactoring

--- cat.js
+++ cat.js
@@ -1,31 +1,82 @@
-data = {
-    name: '',
-    fname: '',
-    numClicks: 0
-}
-var names = ['mewie', 'randolph', 'josephine', 'sammy'];
- var cats = [];
- var baseFname = 'kitty';
- var current = null;
- var cat = function (i) {
-     this.id = i;
-     this.fname = baseFname + i + '.jpg';
-     this.nclicks = 0;
-     this.name = names[i]
- }
- $.each(names, function (i, e) {
-     var c = new cat(i);
-     $('#list').append('<li><a class="catlink" href="#" data-id="' + i + '">' + c.name + '</a></li>');
-     cats.push(c);
- });
- $('.catlink').click(function () {
-     current = $(this).data('id');
-     var cat = cats[current];
-     $('#cat').find('img').attr('src', cat.fname);
-     $('#name').text(cat.name);
-     $('#nclicks').text(cat.nclicks);
- })
- $('#cat').find('img').click(() => {
-     cats[current].nclicks++;
-     $('#nclicks').text(cats[current].nclicks);
- });
+$(function () {
+    data = {
+        cats: [],
+        baseFname: '.jpg',
+        cat: function (id, name) {
+            this.name = name;
+            this.id = id;
+            this.numClicks = 0;
+            this.fname = id + this.baseFname;
+        },
+        addCat: function (dict) {
+            dict = dict instanceof Array ? dict : [dict];
+            $.each(dict, (i, e) => {
+                this.cats.push(new this.cat(e.id, e.name));
+            });
+        }
+    }
+
+    control = {
+        names: ['mewie', 'randolph', 'josephine', 'sammy'],
+        activeCat: -1,
+        init: function () {
+            var dict = this.names.map((function () {
+                var id = 0;
+                var mapper = function (name) {
+                    return {
+                        name: name,
+                        id: id++
+                    };
+                }
+                return mapper;
+            })());
+            data.addCat(dict);
+            listView.init(dict);
+            portraitView.init();
+        },
+        clickPic: function () {
+            data.incrementClick(activeCat);
+            portraitView.setClicks(data.getCat(this.activeCat).numClicks);
+        },
+        clickLI: function (id) {
+            this.activeCat = id;
+            portraitView.render(data.getCat(id));
+        }
+    }
+
+    listView = {
+        init: function (namesDict) {
+            $.each(namesDict, (i, e) => {
+                var htmlStr = '<li><a href="#" data-id="' +
+                    e.id + '">' +
+                    e.name + '</a></li>';
+                this.list.prepend(htmlStr);
+            });
+            this.list.children('li>a').each(function () {
+                $(this).click(function (e) {
+                    control.clickLI($(this).data('id'));
+                    e.preventDefault();
+                });
+            });
+        },
+        list: $('#list')
+    };
+
+    portraitView = {
+        img: $('#image'),
+        numClicks: $('#nclicks'),
+        name: $('#name'),
+        init: function () {
+
+        },
+        render: function (cat) {
+            this.img.attr('src', cat.fname);
+            this.numClicks.text(cat.numClicks);
+            this.name.text(cat.name);
+        },
+        setClicks: function (n) {
+            this.numClicks.text(n);
+        }
+    };
+    control.init();
+});
